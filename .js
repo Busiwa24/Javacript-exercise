4834eafbf6fdc116ebe922c8309ba7008e984e13
Busiwa24 <busiwaliuma@gmail.com>
2021-07-31 05:51:41 +0200

    Repeat solution

---
+++ .js
@@ -0,0 +1,17 @@
+function repeatStringNumTimes(string, times) {
+    var repeatedString = "";
+    while (times > 0) {
+      repeatedString += string;
+      times--;
+    }
+    return repeatedString;
+  }
+  repeatStringNumTimes("abc", 3);function repeatStringNumTimes(string, times) {
+      var repeatedString = "";
+      while (times > 0) {
+        repeatedString += string;
+        times--;
+      }
+      return repeatedString;
+    }
+    repeatStringNumTimes("abc", 3);

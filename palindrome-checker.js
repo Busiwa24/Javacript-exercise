92e38d3de0fa8b2999529ff348ff0ea2983024e4
Busiwa24 <busiwaliuma@gmail.com>
2021-07-31 07:06:24 +0200

    Solve challenge

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -6,12 +6,3 @@ function repeatStringNumTimes(string, times) {
   }
   return repeatedString;
 }
-repeatStringNumTimes("abc", 3);function repeatStringNumTimes(string, times) {
-    var repeatedString = "";
-    while (times > 0) {
-      repeatedString += string;
-      times--;
-    }
-    return repeatedString;
-  }
-  repeatStringNumTimes("abc", 3);
